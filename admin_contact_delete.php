adfdf6b73ddd083a13f716d43fdf14e08297dd99
SherlockPrime <37944021+SherlockPrime@users.noreply.github.com>
2022-06-02 18:01:20 +0900

    Update admin_contact_delete.php

--- admin_contact_delete.php
+++ admin_contact_delete.php
@@ -14,13 +14,19 @@ $member1= mysqli_fetch_array($result1);
 
 if($member1['sJob'] == NULL)
 {
-		echo "<script>"."windosw.alert('관리자나 직원만 접속 가능합니다.');"."location.href='index_log.php';"."</script>";
+		echo "<script>"."window.alert('관리자나 직원만 접속 가능합니다.');"."location.href='index_log.php';"."</script>";
 	}
 
-	$id= $_POST["id"];
-	$query="delete from boardtbl where boardNum = $id ";
+	$name= $_POST["name"];
+
+	$query="delete from boardcmttbl where boardTBL_boardNum = $name";
+
+	$query1="delete from boardtbl where boardNum = $name";
+
 
 	$result= mysqli_query($connect,$query);
+	$result1= mysqli_query($connect,$query1);
+
 
 	echo "<script>"."location.href='admin_contact.php';"."</script>";
 
